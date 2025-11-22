821311a9c597db080f9d399e5a7089c5b701851f
Argenis Lopez <argenislopez28708256@gmail.com>
2025-11-22 01:53:51 -0300

    Fix deploy and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
-  // IMPORTANTE: Isso corrige a tela branca no GitHub Pages.
-  // O valor deve ser o nome do seu repositório entre barras.
-  base: '/Portf-lio/', 
-  server: {
-    open: true, // Abre o navegador automaticamente ao rodar npm start
-  }
-})
+});
